cce2bfd47de262e381d3a42ca11f4f8ed2360d4a
roqe <octopusjune@gmail.com>
2022-10-18 08:50:31 +0000

    code refinement

--- R/pre_fit_one.R
+++ R/pre_fit_one.R
@@ -1,32 +1,18 @@
 #' @export
 pre_fit_one=function(M,Y,X){
-  if(anyNA(X)){
-    print("warning: missing covariate data")
-    X<-matrix(rep(1, length(Y)))
-  }
   if(all(Y %in% 0:1)){
-    fit.y<-glm(Y~.+M, family="binomial", data = X)
+    fit.y<-glm(Y~., family="binomial", data = cbind(M,X))
   }else{
-    fit.y<-lm(Y~.+M, data = X)
+    fit.y<-lm(Y~., data = cbind(M,X))
   }
 
   fit.y.sum<-summary(fit.y)
-  lengX=0
-  for(x in 1:ncol(X)){
-    lengX=lengX+1
-    if(class(X[,x])=="factor"){
-      lengX=lengX+(length(levels(X[,x]))-2)
-    }
-  }
-
-  indB=(lengX+2):nrow(fit.y.sum$coefficients)
+  indB=2:(ncol(M)+1)
   b.hat<-fit.y.sum$coefficients[indB, 1]
   b.sd<-fit.y.sum$coefficients[indB, 2]
   b=b.hat/b.sd
-
   indB=which(!is.na(fit.y$coefficients))[indB]
   covB=as.matrix(vcov(fit.y)[indB,indB])
-
   cb.svd<-svd(covB)
   b.sum<-sum(t(b.hat)%*%cb.svd$u%*%diag(1/sqrt(cb.svd$d),ncol=length(b.hat)))
   corrB=cov2cor(covB)
